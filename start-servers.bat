7dec2fe967d90417aa9c95b82f6915174b1fbd5d
riyagin <102507150+riyagin@users.noreply.github.com>
2026-05-19 11:47:09 +0700

    Update start-servers.bat

--- start-servers.bat
+++ start-servers.bat
@@ -1,3 +1,17 @@
 @echo off
-start "CMS Backend" cmd /k "cd /d "D:\SISTEM BARU JOSHUA\Clinic-Patient-Management-System" && call Scripts\activate.bat && python manage.py runserver 0.0.0.0:8000"
-start "CMS Frontend" cmd /k "cd /d "D:\SISTEM BARU JOSHUA\CPMS-Webapp" && npm run dev -- --host"
+
+if "%1"=="backend" (
+    cd /d "D:\SISTEM BARU JOSHUA\Clinic-Patient-Management-System"
+    call Scripts\activate.bat
+    python manage.py runserver 0.0.0.0:8000
+    exit
+)
+
+if "%1"=="frontend" (
+    cd /d "D:\SISTEM BARU JOSHUA\CPMS-Webapp"
+    npm run dev -- --host
+    exit
+)
+
+start "CMS Backend" cmd /k "%~f0" backend
+start "CMS Frontend" cmd /k "%~f0" frontend
